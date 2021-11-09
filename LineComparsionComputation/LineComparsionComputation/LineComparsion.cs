90f3c09954589fdcad4f1cb9b727ed55c0a49a40
suraj9982 <surajsk9982@gmail.com>
2021-11-09 12:17:00 +0530

    [suraj] add : check length of line

---
+++ LineComparsionComputation/LineComparsionComputation/LineComparsion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparsionComputation
+{
+    class LineComparsion
+    {
+        double xOne, xTwo, yOne, yTwo;
+        public LineComparsion(double xOne,double xTwo,double yOne,double yTwo)
+        {
+            this.xOne = xOne;
+            this.xTwo = xTwo;
+            this.yOne = yOne;
+            this.yTwo = yTwo;
+        }
+        public void checklength()
+        {
+            Console.WriteLine("length of a line is " + Math.Sqrt(Math.Pow(this.xTwo - this.xOne, 2) + (Math.Pow(this.yTwo - this.yOne, 2))));
+        }
+    }
+}
--- LineComparsionComputation/LineComparsionComputation/Program.cs
+++ LineComparsionComputation/LineComparsionComputation/Program.cs
@@ -7,6 +7,8 @@ namespace LineComparsionComputation
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to Line Comaprsion Computation");
+            LineComparsion length = new LineComparsion(2.0,3.0,1.0,2.0);
+            length.checklength();
         }
     }
 }
