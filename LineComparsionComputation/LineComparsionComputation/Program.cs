18eb26f27f37f694c5ad5ddd74add78f0ebf7f88
suraj9982 <surajsk9982@gmail.com>
2021-11-09 12:00:27 +0530

    [suraj] add : line comaprsion computation

---
+++ LineComparsionComputation/LineComparsionComputation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparsionComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Line Comaprsion Computation");
+        }
+    }
+}
